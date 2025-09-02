1e60a852cdd01b5ee1d957686efe955da416272a
Gabriel_Stundner <gabriel.stundner@dell.com>
2025-09-02 00:32:23 -0300

    First Commit

---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import { ion } from 'starlight-ion-theme';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'TermDocs',
+      plugins: [ion()],
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/F4NT0/TermDoc' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
